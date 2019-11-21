096c7b956af859f34b0b88d3bbc73b04479e1ac2
Radomir Tomovic <rtomovic5@gmail.com>
2019-11-21 11:42:49 +0100

    nav-bar

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,16 @@
 import '../scss/style.scss';
+let nav = [
+    {name: 'Home', link: 'index.html'},
+    {name: 'Archive', link: 'archive.html'},
+    {name: 'Pages', link: 'pages.html'},
+    {name: 'Contact', link: 'contact.html'}
+];
+let navList = document.getElementsByClassName("navbar-nav")[0];
+let listElements = "";
+for (let i = 0; i < nav.length; i++){
+    listElements += `<li class = "nav-item"><a class = "nav-link" href = "${nav[i].link}">${nav[i].name}</a></li>`;
+}
+navList.innerHTML = listElements;
+
 
 import $ from 'jquery';
---
+++ src/ts/obj/navObj.ts
@@ -0,0 +1,6 @@
+export let nav = [
+    {name: 'Home', link: 'index.html'},
+    {name: 'Archive', link: 'archive.html'},
+    {name: 'Pages', link: 'pages.html'},
+    {name: 'Contact', link: 'contact.html'}
+];
